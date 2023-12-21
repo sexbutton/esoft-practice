a7ac3edf23b322990448c330da462d9382dce323
vlad <vladik008481@gmail.com>
2023-12-21 14:43:53 +0500

    1

--- frontend-react/src/components/Status/Status.jsx
+++ frontend-react/src/components/Status/Status.jsx
@@ -1,18 +1,18 @@
 import styles from "./Status.module.css";
 import cn from "classnames";
 
-export function Status({children, type}) {
+export function Status({children, type, status}) {
     if (type == "activePlayers"){
-        if (type == "true"){
-            return <span className={cn(styles.activeStatus)}>{children}</span>
+        if (status == "true"){
+            return <span className={cn(styles.activeStatus, styles.ready)}>{children}</span>
         }
         else {
-            return <span className={cn(styles.activeStatus)}>{children}</span>
+            return <span className={cn(styles.activeStatus, styles.inGame)}>{children}</span>
         }
         
     }
     else if (type == "listPlayers"){
-        if (type == "true"){
+        if (status == "true"){
             return <span className={styles.banStatus}>{children}</span>
         }
         else {
